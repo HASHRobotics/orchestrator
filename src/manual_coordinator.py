4a8652a70a12e4d50a6202305b13b89e8ea745fb
Sohil-AK2 <savla.sohil@gmail.com>
2019-04-28 22:37:22 +0000

    Checking button

--- src/manual_coordinator.py
+++ src/manual_coordinator.py
@@ -6,25 +6,22 @@ from bearing_estimator.srv import estimate_bearing,estimate_range,ground_truth_b
 class ManualCoordinator:
     def __init__(self):
         # Range and bearing calculation service
-        self.estimate_bearing = rospy.ServiceProxy('estimate_bearing', estimate_bearing)
+        self.estimate_bearing = rospy.ServiceProxy('/ak1/estimate_bearing', estimate_bearing)
         self.estimate_range = rospy.ServiceProxy('estimate_range', estimate_range)
         self.ground_truth_range = rospy.ServiceProxy('ground_truth_range', ground_truth_range)
-        self.ground_truth_bearing = rospy.ServiceProxy('ground_truth_bearing')
+        self.ground_truth_bearing = rospy.ServiceProxy('/ak1/ground_truth_bearing', ground_truth_bearing)
         # self.addBearingRangeNodes = rospy.ServiceProxy('addBearingRangeNodes', addBearingRangeNodes)
 
         # # optimize pose service
         # self.optimizeFactorGraph = rospy.ServiceProxy('optimizeFactorGraph', optimizeFactorGraph)
 
-        # Listening to Joystick to call services
-        rospy.Subscriber("/ak1/joy", Joy,
-                                    self.take_action)
-
-    def take_action(self, msg):
-        if int(msg.axes[1]) == 1:
-            self.ground_truth_range()
-            self.ground_truth_bearing()
-            self.estimate_range()
-            self.estimate_bearing()
+    def take_action(self,msg):
+        if int(msg.buttons[1]) == 1:
+            rospy.logwarn("Button Pressed")
+            #self.ground_truth_range()
+            #self.ground_truth_bearing()
+            #self.estimate_range()
+            #self.estimate_bearing()
         # elif int(msg.axes[4]) == 1:
         #     self.optimizeFactorGraph()
 
@@ -35,6 +32,10 @@ if __name__ == "__main__":
 
         # Instatiated the class
         NC = ManualCoordinator()
+        
+        while(not rospy.is_shutdown()):
+            msg = rospy.wait_for_message("/ak2/joy", Joy)
+            NC.take_action(msg)
 
         # Wait
         rospy.spin()
